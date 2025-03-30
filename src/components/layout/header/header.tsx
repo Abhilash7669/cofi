0b4ec9157d4db7cbb3c367934cc9c9a1ed0c883c
Abhilash7669 <abhilashsk1998@gmail.com>
2025-03-30 18:06:36 +0530

    theme switcher update

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -8,27 +8,32 @@ type Props = object;
 export default function Header({ }: Props) {
     
   return (
-    <header className="w-full py-2 bg-secondary drop-shadow-lg">
+    <header className="w-full py-2 bg-secondary/50 backdrop-blur-lg drop-shadow-lg sticky top-0 z-50">
       <div className="w-[92%] mx-auto flex items-center justify-between">
         <div className="flex items-center gap-12">
           <CofiLogo className="h-auto w-28" />
-          <ul className="flex items-center gap-4">
-            <li className="text-accent flex items-center gap-1 cursor-pointer">
-              Shop{" "}
-              <FaChevronDown
-                className="text-accent relative top-[0.1rem]"
-                size={12}
-              />
-            </li>
+          <ul className="hidden lg:flex lg:items-center lg:gap-4">
             <li>
               <Button
                 variant="link"
-                className="text-accent decoration-accent"
+                className="text-accent flex items-center gap-1 cursor-pointer"
               >
+                Shop
+                <FaChevronDown
+                  className="relative top-[0.1rem]"
+                />
+              </Button>
+            </li>
+            <li>
+              <Button variant="link" className="text-accent decoration-accent">
                 Blog
               </Button>
             </li>
-            <li className="text-accent cursor-pointer">About</li>
+            <li>
+              <Button variant="link" className="text-accent decoration-accent">
+                About
+              </Button>
+            </li>
           </ul>
         </div>
         <ThemeSwitcher />
--- src/components/layout/header/theme-switcher.tsx
+++ src/components/layout/header/theme-switcher.tsx
@@ -1,8 +1,28 @@
 "use client"
 
+import Image from "next/image";
+import Moon from "@/assets/moon_icon.svg";
+import { MdOutlineLightMode } from "react-icons/md";
+import { useEffect, useRef } from "react";
+
 type Props = object;
 
 export default function ThemeSwitcher({ }: Props) {
+
+    // todo re-work theme switcher
+
+    const iconContainerRef = useRef<HTMLDivElement | null>(null);
+    const sunRef = useRef<HTMLDivElement | null>(null);
+    const moonRef = useRef<HTMLImageElement | null>(null);
+
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            const themeAttribute = document.body.getAttribute("data-theme");
+            if (themeAttribute === "light") {
+                moonRef.current?.classList.add("opacity-0");
+            }
+        }
+    }, []);
 
     function handleTheme(e: React.MouseEvent<HTMLButtonElement>): void {
 
@@ -14,13 +34,50 @@ export default function ThemeSwitcher({ }: Props) {
 
             if (themeAttribute === "light") {
                 document.body.setAttribute("data-theme", "dark");
+                iconContainerRef.current?.classList.add("to-right");
+                iconContainerRef.current?.classList.remove("to-left");
+
+                sunRef.current?.classList.remove("opacity-100");
+                sunRef.current?.classList.add("opacity-0");
+
+                moonRef.current?.classList.add("opacity-100");
+                moonRef.current?.classList.remove("opacity-0");
             } else {
                 document.body.setAttribute("data-theme", "light");
+                iconContainerRef.current?.classList.remove("to-right");
+                iconContainerRef.current?.classList.add("to-left");
+
+                sunRef.current?.classList.remove("opacity-0");
+                sunRef.current?.classList.add("opacity-100");
+
+                moonRef.current?.classList.remove("opacity-100");
+                moonRef.current?.classList.add("opacity-0");
             }
 
         }
 
     }   
 
-    return <button className="text-primary cursor-pointer" type="button" onClick={handleTheme}>Toggle Theme</button>
+    return (
+      <button
+        onClick={handleTheme}
+        className="relative h-9 w-20 rounded-full bg-primary cursor-pointer
+            shadow-[inset_0_-2px_2px_0_rgba(76,47,16,0.25),inset_0_4px_4px_0_rgba(196,154,108,0.25),0_0_10px_2px_rgba(196,154,108,0.25)]"
+      >
+        <div
+          ref={iconContainerRef}
+          className="h-[72%] w-9 absolute top-2/4 -translate-y-2/4"
+        >
+          <div className="h-full w-full relative">
+            <Image ref={moonRef} fill className="transition-all" src={Moon} alt="Moon" />
+            <div
+                ref={sunRef}            
+                className="absolute h-full w-full transition-all"
+            >
+              <MdOutlineLightMode className="text-white h-full w-full" />
+            </div>
+          </div>
+        </div>
+      </button>
+    );
 }
